e8bb60107919ce1ba488b2b137b4dcd02e66f6a0
Akul Sharma <akul.sharma007@gmail.com>
2026-08-11 17:22:29 +0200

    Add: copy-linked-list-with-random-pointer - submission-1

---
+++ Data Structures & Algorithms/copy-linked-list-with-random-pointer/submission-1.ts
@@ -0,0 +1,40 @@
+// class Node {
+//   constructor(val, next = null, random = null) {
+//       this.val = val;
+//       this.next = next;
+//       this.random = random;
+//   }
+// }
+
+class Solution {
+    /**
+     * @param {Node} head
+     * @return {Node}
+     */
+    copyRandomList(head: Node | null): Node {
+        const oldToNew: Map<Node, Node> = new Map();
+        let current = head;
+
+        while (current) {
+            const newNode = new Node(current.val);
+            oldToNew.set(current, newNode);
+            current = current.next;
+        }
+
+        current = head;
+        // Dummy node
+        const result = new Node(-1000);
+        let resultCurrent = result;
+
+        while (current) {
+            const newNode = oldToNew.get(current);
+            resultCurrent.next = newNode;
+            const newRandom = oldToNew.get(current.random) || null;
+            newNode.random = newRandom;
+            current = current.next;
+            resultCurrent = resultCurrent.next;
+        }
+
+        return result.next;
+    }
+}
